63ab7dd0f887b633d41854cf4ea8e3ed13b6370b
George Delosa <georgedel1@outlook.com>
2018-09-29 19:17:04 +1000

    Updated background colours

--- GeoApp/GeoApp/MainPage.xaml.cs
+++ GeoApp/GeoApp/MainPage.xaml.cs
@@ -15,7 +15,9 @@ namespace GeoApp {
         void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
             var item = e.SelectedItem as MasterPageItem;
             if (item != null) {
-                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType)) {
+                    BarBackgroundColor = Color.FromHex("#202225")
+                };
                 masterPage.listView.SelectedItem = null;
                 IsPresented = false;
             }
