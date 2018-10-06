1e445c7b28ad954661726fd65a616e8da631bb3d
George Delosa <georgedel1@outlook.com>
2018-10-06 18:23:42 +1000

    '+' button in the DataEntryList page now navigates to the form page

--- GeoApp/GeoApp/ViewModels/DataEntryListViewModel.cs
+++ GeoApp/GeoApp/ViewModels/DataEntryListViewModel.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace GeoApp
 {
     class DataEntryListViewModel : INotifyPropertyChanged {
         public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand ButtonClickedCommand { set; get; }
 
+        public DataEntryListViewModel() {
+            ButtonClickedCommand = new Command(() =>
+            {
+                MainPage.Instance.ShowDetailFormPage();
+            });
+        }
     }
 }
