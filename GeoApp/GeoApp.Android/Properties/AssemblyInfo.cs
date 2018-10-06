b3b4e05ad4dcf4bf23986b21c7c6a67c58a44368
Ari <luangamath.ari@gmail.com>
2018-10-06 20:34:01 +1000

    Enabled Geolocation functionality

--- GeoApp/GeoApp.Android/Properties/AssemblyInfo.cs
+++ GeoApp/GeoApp.Android/Properties/AssemblyInfo.cs
@@ -16,6 +16,13 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
+// Permissions to allow access to Geolocation functionality
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
