4c490bd96e1de3fbf15c722f9f2a066f64be4291
George Delosa <georgedel1@outlook.com>
2018-10-10 23:14:18 +1000

    Updated metadata fields UI (keyboard type still not working)

---
+++ GeoApp/GeoApp/Models/MetadataXamlLabel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoApp
+{
+    public class MetadataXamlLabel
+    {
+        public string LabelTitle { get; set; }
+        public string KeyboardEntryType { get; set; }
+
+        public MetadataXamlLabel() { }
+
+        public MetadataXamlLabel(string labelTitle, string keyboardEntryType) {
+            this.LabelTitle = labelTitle;
+            this.KeyboardEntryType = keyboardEntryType;
+        }
+    }
+}
--- GeoApp/GeoApp/ViewModels/DetailFormViewModel.cs
+++ GeoApp/GeoApp/ViewModels/DetailFormViewModel.cs
@@ -10,6 +10,7 @@ using Xamarin.Forms;
 using Rg.Plugins.Popup.Extensions;
 using Rg.Plugins.Popup.Services;
 using GeoApp.Views.Popups;
+using System.Collections.ObjectModel;
 
 namespace GeoApp {
     class DetailFormViewModel : INotifyPropertyChanged {
@@ -24,10 +25,6 @@ namespace GeoApp {
         private string _dateEntry;
         private bool _isAddBtnVisible;
 
-        private bool[] _metadataEntriesVisible = new bool[5];
-        private string[] _metadataEntriesLabel = new string[5];
-        private string[] _metadataEntriesType = new string[5];
-
         private bool _addFieldsBtnEnabled;
         private string _addBtnTxt;
         private int[] _gridRow = new int[2];
@@ -35,84 +32,7 @@ namespace GeoApp {
 
         private int numCustomFields = 0;
 
-
-        #region MetadataEntry Properties
-
-        public bool MetadataEntry1Visible {
-            get { return _metadataEntriesVisible[0]; }
-            set { _metadataEntriesVisible[0] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry1Visible")); }
-        }
-
-        public bool MetadataEntry2Visible {
-            get { return _metadataEntriesVisible[1]; }
-            set { _metadataEntriesVisible[1] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry2Visible")); }
-        }
-
-        public bool MetadataEntry3Visible {
-            get { return _metadataEntriesVisible[2]; }
-            set { _metadataEntriesVisible[2] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry3Visible")); }
-        }
-
-        public bool MetadataEntry4Visible {
-            get { return _metadataEntriesVisible[3]; }
-            set { _metadataEntriesVisible[3] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry4Visible")); }
-        }
-
-        public bool MetadataEntry5Visible {
-            get { return _metadataEntriesVisible[4]; }
-            set { _metadataEntriesVisible[4] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry5Visible")); }
-        }
-
-        public string MetadataEntry1Label {
-            get { return _metadataEntriesLabel[0]; }
-            set { _metadataEntriesLabel[0] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry1Label")); }
-        }
-
-        public string MetadataEntry2Label {
-            get { return _metadataEntriesLabel[1]; }
-            set { _metadataEntriesLabel[1] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry2Label")); }
-        }
-
-        public string MetadataEntry3Label {
-            get { return _metadataEntriesLabel[2]; }
-            set { _metadataEntriesLabel[2] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry3Label")); }
-        }
-
-        public string MetadataEntry4Label {
-            get { return _metadataEntriesLabel[3]; }
-            set { _metadataEntriesLabel[3] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry4Label")); }
-        }
-
-        public string MetadataEntry5Label {
-            get { return _metadataEntriesLabel[4]; }
-            set { _metadataEntriesLabel[4] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry5Label")); }
-        }
-
-        public string MetadataEntry1Type {
-            get { return _metadataEntriesType[0]; }
-            set { _metadataEntriesType[0] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry1Type")); }
-        }
-
-        public string MetadataEntry2Type {
-            get { return _metadataEntriesType[1]; }
-            set { _metadataEntriesType[1] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry2Type")); }
-        }
-
-        public string MetadataEntry3Type {
-            get { return _metadataEntriesType[2]; }
-            set { _metadataEntriesType[2] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry3Type")); }
-        }
-
-        public string MetadataEntry4Type {
-            get { return _metadataEntriesType[3]; }
-            set { _metadataEntriesType[3] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry4Type")); }
-        }
-
-        public string MetadataEntry5Type {
-            get { return _metadataEntriesType[4]; }
-            set { _metadataEntriesType[4] = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MetadataEntry5Type")); }
-        }
-        #endregion
+        public ObservableCollection<MetadataXamlLabel> MetadataEntries { get; set; }
 
         public string DateEntry {
             get { return _dateEntry; }
@@ -155,6 +75,8 @@ namespace GeoApp {
         /// <param name="type"></param>
         public DetailFormViewModel(string type) {
             _detailFormPopup = new DetailFormFieldPopup();
+
+            MetadataEntries = new ObservableCollection<MetadataXamlLabel>();
 
             // UI Changes based on selected type
             if (type == "Line" || type == "Polygon") {
@@ -230,40 +152,14 @@ namespace GeoApp {
                 numCustomFields++;
 
                 string keyboardType = "Default";
-                if (result.EntryType != MetaDataTypes.String) {
+                if(result.EntryType != MetaDataTypes.String) {
                     keyboardType = "Numeric";
                 }
 
-                switch (numCustomFields) {
-                    case 1:
-                        MetadataEntry1Visible = true;
-                        MetadataEntry1Label = result.LabelTitle;
-                        MetadataEntry1Type = keyboardType;
-                        break;
-                    case 2:
-                        MetadataEntry2Visible = true;
-                        MetadataEntry2Label = result.LabelTitle;
-                        MetadataEntry2Type = keyboardType;
-                        break;
-                    case 3:
-                        MetadataEntry3Visible = true;
-                        MetadataEntry3Label = result.LabelTitle;
-                        MetadataEntry3Type = keyboardType;
-                        break;
-                    case 4:
-                        MetadataEntry4Visible = true;
-                        MetadataEntry4Label = result.LabelTitle;
-                        MetadataEntry4Type = keyboardType;
-                        break;
-                    case 5:
-                        MetadataEntry5Visible = true;
-                        MetadataEntry5Label = result.LabelTitle;
-                        MetadataEntry5Type = keyboardType;
-
-                        AddFieldsBtnEnabled = false;
-                        break;
-                    default:
-                        break;
+                MetadataEntries.Add(new MetadataXamlLabel(result.LabelTitle, keyboardType));
+                
+                if(numCustomFields == 5) {
+                    AddFieldsBtnEnabled = false;
                 }
             }
         }
