9a6d8a49c8e03338ca2bcf218194aca0a91184fd
Ari <luangamath.ari@gmail.com>
2018-10-23 14:03:17 +1000

    Locked application orientation to portrait

--- GeoApp/GeoApp.Android/MainActivity.cs
+++ GeoApp/GeoApp.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Xamarin.Essentials;
 using Debug = System.Diagnostics.Debug;
 
 namespace GeoApp.Droid {
-    [Activity(Label = "GeoApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "GeoApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
         protected override void OnCreate(Bundle bundle) {
             TabLayoutResource = Resource.Layout.Tabbar;
