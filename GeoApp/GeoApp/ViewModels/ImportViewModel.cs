ed77a66a7972d92b46f035ed1d598536f60be511
Ari <luangamath.ari@gmail.com>
2018-10-25 05:31:07 +1000

    Implemented import functionality (TODO: Properly extract data and save to internal storage)

--- GeoApp/GeoApp/ViewModels/ImportViewModel.cs
+++ GeoApp/GeoApp/ViewModels/ImportViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Plugin.FilePicker;
 using Plugin.FilePicker.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using System;
 using System.Diagnostics;
 using System.Windows.Input;
@@ -12,15 +14,29 @@ namespace GeoApp {
         public ImportViewModel() {
             ButtonClickCommand = new Command(async () => {
                 try {
-                    FileData fileData = await CrossFilePicker.Current.PickFile();
-                    if (fileData == null)
-                        return; // user canceled file picking
+                    var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+                    if (status != PermissionStatus.Granted) {
+                        if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Storage)) {
+                            await HomePage.Instance.DisplayAlert("File", "Need that file", "OK");
+                        }
 
-                    string fileName = fileData.FileName;
-                    string contents = System.Text.Encoding.UTF8.GetString(fileData.DataArray);
+                        var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Storage);
+                        if (results.ContainsKey(Permission.Storage)) {
+                            status = results[Permission.Storage];
+                        }
+                    }
 
-                    Debug.WriteLine("File name chosen: " + fileName);
-                    Debug.WriteLine("File data: " + contents);
+                    if (status == PermissionStatus.Granted) {
+                        FileData fileData = await CrossFilePicker.Current.PickFile();
+                        if (fileData == null)
+                            return; // user canceled file picking
+
+                        string fileName = fileData.FileName;
+                        string contents = System.Text.Encoding.UTF8.GetString(fileData.DataArray); // TODO: NEED TO PROPERLY EXTRACT DATA FROM FILE
+
+                        Debug.WriteLine("File name chosen: " + fileName);
+                        Debug.WriteLine("File data: " + contents);
+                    }
                 } catch (Exception ex) {
                     Debug.WriteLine("Exception choosing file: " + ex.ToString());
                 }
