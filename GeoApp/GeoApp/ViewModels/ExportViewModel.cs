1ff5498fcd4aebcbd7b931e41680b03699d2a500
DESKTOP-GFC4C86\Asus <faiyaz.samiul.haque@gmail.com>
2018-10-24 20:56:11 +1000

    Changed mvc to mvvm for export

---
+++ GeoApp/GeoApp/ViewModels/ExportViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Plugin.Share;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GeoApp
+{
+    class ExportViewModel
+    {
+        public ICommand ButtonClickCommand { set; get; }
+
+        public ExportViewModel()
+        {
+            
+            ButtonClickCommand = new Command( () =>
+            {
+                CrossShare.Current.Share(new Plugin.Share.Abstractions.ShareMessage
+                {
+                    Text = "my clipboard",
+                    Title = "Share"
+                });
+            });
+        }
+        
+    }
+
+   
+}
