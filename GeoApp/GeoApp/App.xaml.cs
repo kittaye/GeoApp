0347a4708fe9479e6f54c663169dfa81d44f581a
Reilly MacKenzie-Cree <n9966471@qut.edu.au>
2018-10-24 21:37:50 +1000

    Updated UI and manifest files

--- GeoApp/GeoApp/App.xaml.cs
+++ GeoApp/GeoApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace GeoApp {
 
         public App() {
             InitializeComponent();
-            MainPage = new NavigationPage(HomePage.Instance) { BarBackgroundColor = Color.FromHex("202225") };
+            MainPage = new NavigationPage(HomePage.Instance) { BarBackgroundColor = Color.White };
             LocationManager = new LocationItemManager(new FileService());
         }
 
