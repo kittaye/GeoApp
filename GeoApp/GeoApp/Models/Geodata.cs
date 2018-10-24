b5ad8ac2fa2ac61cf70ce99a529946c61fda5d35
Reilly MacKenzie-Cree <n9966471@qut.edu.au>
2018-10-24 13:16:07 +1000

    Updated data model

--- GeoApp/GeoApp/Models/Geodata.cs
+++ GeoApp/GeoApp/Models/Geodata.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GeoApp
 {
-    public enum DataType { Point, Line, Polygon };
+    public enum DataType { Point, Line, Polygon }; //Change Line to LineString? Also potential for Multipart Gemoetries: MultiPoint, MultiLineString and MultiPolygon.
 
     public class Feature
     {
@@ -18,13 +18,14 @@ namespace GeoApp
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string TypeIcon { get; set; }
+        public string Details { get; set; }
+       // public string TypeIcon { get; set; }
     }
 
     public class Geometry
     {
         public DataType Type { get; set; }
-        public List<double> Coordinates { get; set; }
+        public List<double> Coordinates { get; set; } //Longtitute, Latitude, Elevation
     }
 
 
