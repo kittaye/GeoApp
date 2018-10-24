299174976c73ee0a27dc50f8cb21c75b0cefc722
Ari <luangamath.ari@gmail.com>
2018-10-25 03:29:58 +1000

    Implemented import function (WIP)

--- GeoApp/GeoApp/ViewModels/ExportViewModel.cs
+++ GeoApp/GeoApp/ViewModels/ExportViewModel.cs
@@ -5,26 +5,20 @@ using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
-namespace GeoApp
-{
-    class ExportViewModel
-    {
+namespace GeoApp {
+    class ExportViewModel {
         public ICommand ButtonClickCommand { set; get; }
 
-        public ExportViewModel()
-        {
-            
-            ButtonClickCommand = new Command( () =>
-            {
-                CrossShare.Current.Share(new Plugin.Share.Abstractions.ShareMessage
-                {
+        public ExportViewModel() {
+
+            ButtonClickCommand = new Command(() => {
+                CrossShare.Current.Share(new Plugin.Share.Abstractions.ShareMessage {
                     Text = "my clipboard",
                     Title = "Share"
                 });
             });
         }
-        
     }
 
-   
+
 }
---
+++ GeoApp/GeoApp/ViewModels/ImportViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
+using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GeoApp {
+    class ImportViewModel {
+        public ICommand ButtonClickCommand { set; get; }
+
+        public ImportViewModel() {
+            ButtonClickCommand = new Command(async () => {
+                try {
+                    FileData fileData = await CrossFilePicker.Current.PickFile();
+                    if (fileData == null)
+                        return; // user canceled file picking
+
+                    string fileName = fileData.FileName;
+                    string contents = System.Text.Encoding.UTF8.GetString(fileData.DataArray);
+
+                    Debug.WriteLine("File name chosen: " + fileName);
+                    Debug.WriteLine("File data: " + contents);
+                } catch (Exception ex) {
+                    Debug.WriteLine("Exception choosing file: " + ex.ToString());
+                }
+            });
+        }
+    }
+}
