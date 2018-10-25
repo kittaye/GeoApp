c314830d06013a1db91cc1f9bf1408f52128955b
Reilly MacKenzie-Cree <n9966471@qut.edu.au>
2018-10-26 02:29:33 +1000

    Attempt to get pins on map

--- GeoApp/GeoApp/Views/MapView.xaml.cs
+++ GeoApp/GeoApp/Views/MapView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,20 +12,61 @@ using Xamarin.Forms.Maps;
 namespace GeoApp {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapView : ContentPage {
-        public MapView() {
+        Map map;
+        public MapView()
+        {
             InitializeComponent();
-            var map = new Map(
-            MapSpan.FromCenterAndRadius(
-                    new Position(37, -122), Distance.FromMiles(0.3)))
-                         {
-                             IsShowingUser = true,
-                             HeightRequest = 100,
-                             WidthRequest = 960,
-                             VerticalOptions = LayoutOptions.FillAndExpand
-                         };
-                    var stack = new StackLayout { Spacing = 0 };
-                    stack.Children.Add(map);
-                    Content = stack;
+
+            map = new Map(
+            MapSpan.FromCenterAndRadius(new Position(-27.4784849, 153.0262424), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+
         }
+
+
+
+        public void Locc()
+        {
+            List<Feature> features = new List<Feature>();
+            features = App.LocationManager.CurrentLocations;
+
+            foreach (var feature in features)
+            {
+                foreach (var point in feature.properties.xamarincoordinates)
+                {
+                    addPin(feature.properties.name, point.Latitude, point.Longitude);
+                    
+                }
+            }
+        }
+
+
+
+        public void addPin(string name, double lat, double longt){
+            var position = new Position(lat, longt); // Latitude, Longitude
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = position,
+                Label = name,
+                Address = "custom detail info"
+            };
+            map.Pins.Add(pin);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Locc();
+        }
+
     }
 }
