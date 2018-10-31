bcfde6c006139c6c5a5e7eb25b0cd29c476825da
Ari <luangamath.ari@gmail.com>
2018-10-31 15:45:57 +1000

    Initial DetailFormViewModel unit tests

---
+++ GeoApp.Tests/DetailFormViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoApp.Tests {
+    [TestFixture]
+    public class DetailFormViewModelTest {
+
+        DetailFormViewModel viewModel;
+        [SetUp]
+        public void SetUp() {
+            viewModel = new DetailFormViewModel("Point");
+        }
+
+        [Test]
+        public void TestMethod() {
+            // TODO: Add your test code here
+            
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
