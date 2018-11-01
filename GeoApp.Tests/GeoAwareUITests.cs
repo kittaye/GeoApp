63e15e467e62b1f7c2d8429c1aacd904eea2a41b
Ari <luangamath.ari@gmail.com>
2018-11-02 01:04:35 +1000

    Added test for tabbed navigation

--- GeoApp.Tests/GeoAwareUITests.cs
+++ GeoApp.Tests/GeoAwareUITests.cs
@@ -21,8 +21,24 @@ namespace GeoApp.Tests {
             app = AppInitializer.StartApp(platform);
         }
 
+        /// <summary>
+        /// Tests tabbed navigation menu
+        /// </summary>
         [Test]
-        public void WelcomeTextIsDisplayed() {
+        public void TestTabbedNavigation() {
+            AppResult[] results = app.Query(c => c.Class("BottomNavigationitemView"));
+
+            app.TapCoordinates(results[1].Rect.X, results[1].Rect.Y); // map view
+            app.TapCoordinates(results[2].Rect.X, results[2].Rect.Y); // import
+            app.TapCoordinates(results[3].Rect.X, results[3].Rect.Y); // export
+            app.TapCoordinates(results[0].Rect.X, results[0].Rect.Y); // data entries
+        }
+
+        /// <summary>
+        /// Remove after unit testing
+        /// </summary>
+        [Test]
+        public void Repl() {
             app.Repl();
         }
     }
