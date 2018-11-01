9878ea1beed5885b3a3bbfb98abbb5779758bd93
Ari <luangamath.ari@gmail.com>
2018-11-02 03:14:49 +1000

    Added test for NavToAddDataEntryDialogBox

--- GeoApp.Tests/GeoAwareUITests.cs
+++ GeoApp.Tests/GeoAwareUITests.cs
@@ -26,16 +26,33 @@ namespace GeoApp.Tests {
         /// </summary>
         [Test]
         public void TestTabbedNavigation() {
-            AppResult[] results = app.Query(c => c.Class("BottomNavigationitemView")); 
-            
-            app.TapCoordinates(results[1].Rect.X, results[1].Rect.Y); // map view
-            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Maps"); // used to capture the current page
-            app.TapCoordinates(results[2].Rect.X, results[2].Rect.Y); // import
-            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Import");
-            app.TapCoordinates(results[3].Rect.X, results[3].Rect.Y); // export
-            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Export");
-            app.TapCoordinates(results[0].Rect.X, results[0].Rect.Y); // data entries
-            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Data Entries");
+            AppResult[] res = app.Query(x => x.Class("BottomNavigationitemView"));
+
+            app.TapCoordinates(res[1].Rect.X, res[1].Rect.Y); // map view
+            Assert.AreEqual((app.Query(x => x.Id("largeLabel")))[0].Text, "Maps"); // used to capture the current page
+            app.TapCoordinates(res[2].Rect.X, res[2].Rect.Y); // import
+            Assert.AreEqual((app.Query(x => x.Id("largeLabel")))[0].Text, "Import");
+            app.TapCoordinates(res[3].Rect.X, res[3].Rect.Y); // export
+            Assert.AreEqual((app.Query(x => x.Id("largeLabel")))[0].Text, "Export");
+            app.TapCoordinates(res[0].Rect.X, res[0].Rect.Y); // data entries
+            Assert.AreEqual((app.Query(x => x.Id("largeLabel")))[0].Text, "Data Entries");
+        }
+
+        [Test]
+        public void TestNavToAddDataEntryDialogBox() {
+            AppResult[] res = app.Query(x => x.Class("ActionMenuItemView"));
+            AppResult[] ress = app.Query(x => x.Class("BottomNavigationitemView"));
+
+            app.TapCoordinates(res[0].Rect.X, res[0].Rect.Y); // tap the add data entry icon (top right)
+            Assert.AreEqual((app.Query(x => x.Id("alertTitle")))[0].Text, "Select a Data Type"); // check if the add button displays an alertbox
+
+            // perform query on the current page to identify the dialogoptions
+            //var dialogOptions = app.Query("text1"); // 0 = Point , 1 = Line , 2 = Polygon
+            // select Point data type
+            //app.TapCoordinates(ress[3].Rect.X, ress[3].Rect.Y); // export
+            //app.TapCoordinates(dialogOptions[0].Rect.X, dialogOptions[0].Rect.Y);
+            // Verify that we're in New Point page
+            //Assert.AreEqual(app.Query(c => c.Class("AppCompatTextView"))[0].Text, "New Point");
         }
 
         /// <summary>
