57a7c52235c8b4c96d18570885fa114a40bf245b
Ari <luangamath.ari@gmail.com>
2018-11-02 01:32:37 +1000

    Added assertions to tabbed navigation tests

--- GeoApp.Tests/GeoAwareUITests.cs
+++ GeoApp.Tests/GeoAwareUITests.cs
@@ -26,12 +26,16 @@ namespace GeoApp.Tests {
         /// </summary>
         [Test]
         public void TestTabbedNavigation() {
-            AppResult[] results = app.Query(c => c.Class("BottomNavigationitemView"));
-
+            AppResult[] results = app.Query(c => c.Class("BottomNavigationitemView")); 
+            
             app.TapCoordinates(results[1].Rect.X, results[1].Rect.Y); // map view
+            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Maps"); // used to capture the current page
             app.TapCoordinates(results[2].Rect.X, results[2].Rect.Y); // import
+            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Import");
             app.TapCoordinates(results[3].Rect.X, results[3].Rect.Y); // export
+            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Export");
             app.TapCoordinates(results[0].Rect.X, results[0].Rect.Y); // data entries
+            Assert.AreEqual((app.Query(c => c.Id("largeLabel")))[0].Text, "Data Entries");
         }
 
         /// <summary>
