c4c3339c006e1e091dbf05a759df1f744bef7797
Ari <luangamath.ari@gmail.com>
2018-11-01 13:47:55 +1000

    Added preprocessor for platform specific code to be run for MapView; this fixes some instances of Android boot crash

--- GeoApp/GeoApp/ViewModels/MapViewModel.cs
+++ GeoApp/GeoApp/ViewModels/MapViewModel.cs
@@ -4,30 +4,34 @@ using Xamarin.Forms;
 using System.Collections.Generic;
 using System;
 
-namespace GeoApp
-{
+namespace GeoApp {
     /// <summary>
     /// ViewModel for maps page
     /// </summary>
-    public class MapViewModel
-    {
+    public class MapViewModel {
         private CustomMap map;
         public List<Pin> CustomPins { get; set; }
 
-        public MapViewModel()
-        {
+        public MapViewModel() {
+#if __IOS__
             InitialiseMap();
+#endif
+
         }
 
         /// <summary>
         /// Create map and set starting position
         /// </summary>
         /// <returns></returns>
-        public Map InitialiseMap()
-        {
-            map = new CustomMap()
-            {
+        public Map InitialiseMap() {
+            map = new CustomMap() {
+#if __IOS__
                 IsShowingUser = true,
+#else
+#if __ANDROID__
+                IsShowingUser = false,
+#endif
+#endif
                 HeightRequest = 100,
                 WidthRequest = 960,
                 VerticalOptions = LayoutOptions.FillAndExpand,
@@ -39,15 +43,13 @@ namespace GeoApp
         /// <summary>
         /// Adds overlays to the map, with different methods for points, lines and perimeters. Currently only points activated
         /// </summary>
-        public void AddGeometry()
-        {
+        public void AddGeometry() {
             List<Feature> features = App.FeaturesManager.CurrentFeatures;
             map.Pins.Clear();
 
-            foreach (var feature in features)
-            {
-                if(feature.geometry.type == "Point") //Use pins for points
-                { 
+            foreach (var feature in features) {
+                if (feature.geometry.type == "Point") //Use pins for points
+                {
                     CreatePin(feature.properties.name, feature.properties.xamarincoordinates[0].Latitude, feature.properties.xamarincoordinates[0].Longitude);
                 }
                 if (feature.geometry.type == "Line") //
@@ -55,8 +57,8 @@ namespace GeoApp
                     //List<Position> pos = new List<Position> { new Position(feature.properties.xamarincoordinates[0].Latitude, feature.properties.xamarincoordinates[0].Longitude), new Position(feature.properties.xamarincoordinates[1].Latitude, feature.properties.xamarincoordinates[1].Longitude) };
                     //map.ShapeCoordinates.Add(pos);
                 }
-                    if (feature.geometry.type == "Polygon") //Use overlay shapes for perimeters
-                {
+                if (feature.geometry.type == "Polygon") //Use overlay shapes for perimeters
+            {
                     //List<Position> posi = new List<Position> { new Position(39.934633, 116.399921), new Position(39.929709, 116.400208), new Position(39.929792, 116.405994), new Position(39.934689, 116.405526) };
                     //map.ShapeCoordinates.Add(posi);
                 }
@@ -66,8 +68,7 @@ namespace GeoApp
         /// <summary>
         /// Call geometry whenever map comes into view, clearing and readding all data points
         /// </summary>
-        public void RefreshMap()
-        {
+        public void RefreshMap() {
             AddGeometry();
         }
 
@@ -77,11 +78,9 @@ namespace GeoApp
         /// <param name="name">point name</param>
         /// <param name="lat">point latitude</param>
         /// <param name="lon">point longitude</param>
-        public void CreatePin(string name, double lat, double lon)
-        {
+        public void CreatePin(string name, double lat, double lon) {
             var position = new Position(lat, lon); // Latitude, Longitude
-            var pin = new Pin
-            {
+            var pin = new Pin {
                 Type = PinType.Place,
                 Position = position,
                 Label = name,
