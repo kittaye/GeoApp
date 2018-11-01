7764005f45be59ebbf0a06fed70f23f5b8565bab
Ari <luangamath.ari@gmail.com>
2018-11-01 22:40:10 +1000

    Added UITests; work in progress

---
+++ GeoApp.Tests/GeoAwareUITests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace GeoApp.Tests {
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class GeoAwareUITests {
+        IApp app;
+        Platform platform;
+
+        public GeoAwareUITests(Platform platform) {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest() {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed() {
+            app.Repl();
+        }
+    }
+}
