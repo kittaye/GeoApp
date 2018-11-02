1d36a27f036a288b59d899eee0b02954f5e3c45f
Ari <luangamath.ari@gmail.com>
2018-11-02 12:11:19 +1000

    Added tests for TestNavtoDetailFormView

--- GeoApp.Tests/GeoAwareUITests.cs
+++ GeoApp.Tests/GeoAwareUITests.cs
@@ -38,21 +38,32 @@ namespace GeoApp.Tests {
             Assert.AreEqual((app.Query(x => x.Id("largeLabel")))[0].Text, "Data Entries");
         }
 
+        /// <summary>
+        /// Tests navigation to dialog box for selecting a data type for detail form view
+        /// </summary>
         [Test]
         public void TestNavToAddDataEntryDialogBox() {
             AppResult[] res = app.Query(x => x.Class("ActionMenuItemView"));
-            AppResult[] ress = app.Query(x => x.Class("BottomNavigationitemView"));
 
             app.TapCoordinates(res[0].Rect.X, res[0].Rect.Y); // tap the add data entry icon (top right)
             Assert.AreEqual((app.Query(x => x.Id("alertTitle")))[0].Text, "Select a Data Type"); // check if the add button displays an alertbox
+        }
 
-            // perform query on the current page to identify the dialogoptions
-            //var dialogOptions = app.Query("text1"); // 0 = Point , 1 = Line , 2 = Polygon
-            // select Point data type
-            //app.TapCoordinates(ress[3].Rect.X, ress[3].Rect.Y); // export
-            //app.TapCoordinates(dialogOptions[0].Rect.X, dialogOptions[0].Rect.Y);
-            // Verify that we're in New Point page
-            //Assert.AreEqual(app.Query(c => c.Class("AppCompatTextView"))[0].Text, "New Point");
+
+        /// <summary>
+        /// Tests navigation to detail form view (using POINT as an example)
+        /// </summary>
+        [Test]
+        public void TestNavtoDetailFormView() {
+
+            TestNavToAddDataEntryDialogBox();
+
+            //perform query on the current page to identify the dialogoptions
+            var dialogOptions = app.Query("text1"); // 0 = Point , 1 = Line , 2 = Polygon
+            //select Point data type
+            app.TapCoordinates(dialogOptions[0].Rect.CenterX, dialogOptions[0].Rect.CenterY);
+            //Verify that we're in New Point page
+            Assert.AreEqual(app.Query(c => c.Class("AppCompatTextView"))[0].Text, "New Point");
         }
 
         /// <summary>
