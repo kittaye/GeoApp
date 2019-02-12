4ec04aef24a01fa57bb5783bac6becc40a7973e1
Donghyeon Kim <d48.kim@connect.qut.edu.au>
2019-02-12 12:15:32 +1000

    Changed the functionality of exporting
    Instead of exporting in texts of geojson data, it will create a new .json file with encoded data, then email to the entered address

--- GeoApp/GeoApp/ViewModels/ExportViewModel.cs
+++ GeoApp/GeoApp/ViewModels/ExportViewModel.cs
@@ -8,23 +8,97 @@ using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
+/// Mine
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Plugin.FilePicker;
+using Xamarin.Forms.PlatformConfiguration;
+using Plugin.FilePicker.Abstractions;
+
+
 namespace GeoApp {
-    public class ExportViewModel {
+    public class ExportViewModel: ViewModelBase {
+
         public ICommand ButtonClickCommand { set; get; }
+
+
+        private string _EmailEntry;
+        public string EmailEntry
+        {
+            get { return _EmailEntry; }
+            set
+            {
+                _EmailEntry = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         /// View-model constructor for the export page.
         /// </summary>
         public ExportViewModel() {
-            if (!CrossShare.IsSupported)
-                return;
 
             ButtonClickCommand = new Command(async () => {
-                await CrossShare.Current.Share(new ShareMessage {
-                    Text = App.FeaturesManager.ExportFeaturesToJson(),
-                    Title = "Share"
-                });
+
+
+                if (string.IsNullOrWhiteSpace(EmailEntry) == false)
+                {
+                    try
+                    {
+
+                        //JSON_attachment = App.FeaturesManager.ExportFeaturesToJson();
+
+                        string testString = App.FeaturesManager.ExportFeaturesToJson();
+
+                        string email_Address = EmailEntry;
+
+
+                        MailMessage mail = new MailMessage();
+                        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                        mail.From = new MailAddress("GeoApp@gmail.com");
+
+                        mail.To.Add(email_Address);
+
+                        mail.Subject = "Your requested Geo Application CSV dataset";
+                        mail.Body = "The following attachment is your request dataset contained into a GEO JSON format";
+
+
+                        // Attaching attachment files into mails
+                        mail.Attachments.Add(Attachment.CreateAttachmentFromString(testString, "GeoAware.json"));
+                        // mail.Attachments.Add(JSON_attachment);
+
+
+                        SmtpServer.Port = 587;
+                        SmtpServer.Credentials = new System.Net.NetworkCredential("geoapplicationqut@gmail.com", "Geoapplication123");
+                        SmtpServer.EnableSsl = true;
+
+                        ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+                        {
+                            return true;
+                        };
+
+                        SmtpServer.Send(mail);
+
+                        await HomePage.Instance.DisplayAlert("Status", "Email sent successfully", "OK");
+                    }
+
+                    catch (Exception mailNotSent)
+                    {
+                        await HomePage.Instance.DisplayAlert("Status", "Unable to send email. Error :" + mailNotSent, "OK");
+                    }
+                }
+
+                else
+                {
+                    await HomePage.Instance.DisplayAlert("Invalid ID", "The ID cannot be empty.", "OK");
+                }
+
+               
             });
+
+
         }
     }
 }
