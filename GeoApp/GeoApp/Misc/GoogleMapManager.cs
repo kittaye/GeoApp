2f029a38a7f391ed3249258a645059aee265a303
Ari <luangamath.ari@gmail.com>
2019-02-15 01:58:34 +1000

    Fixed application crash when importing data and immediately going to the map page

--- GeoApp/GeoApp/Misc/GoogleMapManager.cs
+++ GeoApp/GeoApp/Misc/GoogleMapManager.cs
@@ -5,29 +5,23 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
 using Xamarin.Essentials;
-using System.Threading.Tasks;
 
-namespace GeoApp
-{
-    public class GoogleMapManager
-    {
-        public static void DropPins(ObservableCollection<Pin> Pins, string name, List<Point> points)
-        {
-            points.ForEach((Point point) =>
-            {
-                Pins.Add(new Pin
-                {
-                    Label = $"{name}",
-                    Address = $"Lat: {point.Latitude} \nLong:{point.Longitude}",
-                    Position = new Position(point.Latitude, point.Longitude)
+namespace GeoApp {
+    public class GoogleMapManager {
+        public static void DropPins(ObservableCollection<Pin> Pins, string name, List<Point> points) {
+            if (points != null) {
+                points.ForEach((Point point) => {
+                    Pins.Add(new Pin {
+                        Label = $"{name}",
+                        Address = $"Lat: {point.Latitude} \nLong:{point.Longitude}",
+                        Position = new Position(point.Latitude, point.Longitude)
+                    });
                 });
-            });
+            }
         }
 
-        public static void DrawLine(ObservableCollection<Polyline> Polylines, string name, List<Point> points)
-        {
-            var line = new Polyline()
-            {
+        public static void DrawLine(ObservableCollection<Polyline> Polylines, string name, List<Point> points) {
+            var line = new Polyline() {
                 StrokeColor = Color.Blue,
                 IsClickable = true,
                 StrokeWidth = 5f,
@@ -38,24 +32,25 @@ namespace GeoApp
             var message = "";
             var index = 0;
 
-            points.ForEach((Point point) =>
-            {
-                index++;
-                line.Positions.Add(new Position(point.Latitude, point.Longitude));
-                message += $"Coordinate{index} : Lat {point.Latitude} , Long {point.Longitude} \n";
-            });
+            if (points != null) {
+                points.ForEach((Point point) => {
+                    index++;
+                    line.Positions.Add(new Position(point.Latitude, point.Longitude));
+                    message += $"Coordinate{index} : Lat {point.Latitude} , Long {point.Longitude} \n";
+                });
+            }
 
             line.Clicked += (sender, e) => {
                 Application.Current.MainPage.DisplayAlert(name, message, "Okay");
             };
 
-            Polylines.Add(line);
+            if (Polylines != null) {
+                Polylines.Add(line);
+            }
         }
 
-        public static void DrawPolygon(ObservableCollection<Polygon> Polygons, string name, List<Point> points)
-        {
-            var polygon = new Polygon()
-            {
+        public static void DrawPolygon(ObservableCollection<Polygon> Polygons, string name, List<Point> points) {
+            var polygon = new Polygon() {
                 StrokeColor = Color.Gray,
                 FillColor = Color.FromRgba(255, 0, 0, 64),
                 IsClickable = true,
@@ -67,19 +62,21 @@ namespace GeoApp
             var message = "";
             var index = 0;
 
-            points.ForEach((Point point) =>
-            {
-                index++;
-                polygon.Positions.Add(new Position(point.Latitude, point.Longitude));
-                message += $"Coordinate{index} : Lat {point.Latitude} , Long {point.Longitude} \n";
-            });
+            if (points != null) {
+                points.ForEach((Point point) => {
+                    index++;
+                    polygon.Positions.Add(new Position(point.Latitude, point.Longitude));
+                    message += $"Coordinate{index} : Lat {point.Latitude} , Long {point.Longitude} \n";
+                });
+            }
 
             polygon.Clicked += (sender, e) => {
                 Application.Current.MainPage.DisplayAlert(name, message, "Okay");
             };
 
-            Polygons.Add(polygon);
+            if (polygon != null) {
+                Polygons.Add(polygon);
+            }
         }
-
     }
 }
