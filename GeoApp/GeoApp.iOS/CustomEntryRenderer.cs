a6aa1c14035aa3211825b600b6cebfa4ae306985
Reilly MacKenzie-Cree <n9966471@qut.edu.au>
2019-02-21 13:58:12 +1000

    Added keyboard toolaber with negative and done buttons for iOS

---
+++ GeoApp/GeoApp.iOS/CustomEntryRenderer.cs
@@ -0,0 +1,79 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.Drawing;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(GeoApp.CustomEntryRenderer))]
+namespace GeoApp
+
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            // Check for only Numeric keyboard
+
+            if (Element == null)
+            {
+                return;
+            }
+
+            if (this.Element.Keyboard == Keyboard.Numeric)
+            {
+                 this.AddNegDoneButton();
+            }
+
+            if (this.Element.Keyboard == Keyboard.Default || this.Element.Keyboard == Keyboard.Text)
+            {
+                this.AddDoneButton();
+            }
+        }
+        /// <summary>
+        /// Add toolbar with Done button
+        /// </summary>
+        /// 
+        /// 
+
+        protected void AddDoneButton()
+        {
+            UIToolbar toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
+            toolbar.Translucent = true;
+
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate {
+                this.Control.ResignFirstResponder();
+            });
+
+            toolbar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+
+                };
+            this.Control.InputAccessoryView = toolbar;
+        }
+
+        protected void AddNegDoneButton()
+        {
+            UIToolbar toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
+            toolbar.Translucent = true;
+
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate {
+                this.Control.ResignFirstResponder();
+            });
+            var negButton = new UIBarButtonItem("-", UIBarButtonItemStyle.Plain, delegate {
+                this.Control.InsertText("-");
+            });
+
+            toolbar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FixedSpace),
+                negButton,
+                new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+                new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+
+                };
+            this.Control.InputAccessoryView = toolbar;
+        }
+    }
+}
