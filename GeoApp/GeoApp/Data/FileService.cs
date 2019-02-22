53280575e9af820d191b418fdbd8186bd670364a
George Delosa <georgedel1@outlook.com>
2019-02-22 21:30:43 +1000

    Fixed invalid GeoJSON imports & added unsupported type alert message

--- GeoApp/GeoApp/Data/FileService.cs
+++ GeoApp/GeoApp/Data/FileService.cs
@@ -66,75 +66,88 @@ namespace GeoApp {
                 rootobject.type = "FeatureCollection";
 
                 foreach (var feature in rootobject.features) {
-                    // Immediately convert LineStrings to Line for use in the rest of the codebase. 
-                    // This will be converted back to LineString before serialization back to json.
-                    if(feature.geometry.type == "LineString") {
-                        feature.geometry.type = "Line";
-                    }
-
-                    // If author ID hasn't been set on the feature, default it to the user's ID.
-                    if (string.IsNullOrWhiteSpace(feature.properties.authorId)) {
-                        if (App.Current.Properties.ContainsKey("UserID")) {
-                            feature.properties.authorId = App.Current.Properties["UserID"] as string;
-                        } else {
-                            feature.properties.authorId = string.Empty;
-                        }
-                    }
-
-                    // If the date field is missing or invalid, convert it into DateTime.Now.
-                    DateTime dummy;
-                    if (feature.properties.date == null || DateTime.TryParse(feature.properties.date, out dummy) == false) {
-                        Debug.WriteLine($"\n\n::::::::::::::::::::::BAD DATE VALUE: {feature.properties.date}, DEFAULTING TO DateTime.Now");
-                        feature.properties.date = DateTime.Now.ToShortDateString();
-                    }
-
-                    // Determine the icon used for each feature based on it's geometry type.
-                    if (feature.geometry.type == "Point") {
-                        feature.properties.typeIconPath = "point_icon.png";
-                    } else if (feature.geometry.type == "Line") {
-                        feature.properties.typeIconPath = "line_icon.png";
-                    } else if (feature.geometry.type == "Polygon") {
-                        feature.properties.typeIconPath = "area_icon.png";
-                    } else {
-                        Debug.WriteLine($"\n\n::::::::::::::::::::::::INVALID TYPE: {feature.geometry.type}");
-                        throw new Exception();
-                    }
-
-                    // Initialise xamarin coordinates list.
-                    feature.properties.xamarincoordinates = new List<Point>();
-
-                    // Properly deserialize the list of coordinates into an app-use-specific list of Points (XamarinCoordinates).
-                    {
-                        object[] trueCoords;
-
-                        if (feature.geometry.type == "Point") {
-                            trueCoords = feature.geometry.coordinates.ToArray();
-                            feature.properties.xamarincoordinates.Add(JsonCoordToXamarinPoint(trueCoords));
-
-                        } else if (feature.geometry.type == "Line") {
-                            // Iterates the root coordinates (List<object>),
-                            // then casts each element in the list to a Jarray which contain the actual coordinates.
-                            for (int i = 0; i < feature.geometry.coordinates.Count; i++) {
-                                trueCoords = ((JArray)feature.geometry.coordinates[i]).ToObject<object[]>();
-                                feature.properties.xamarincoordinates.Add(JsonCoordToXamarinPoint(trueCoords));
-                            }
-                        } else if (feature.geometry.type == "Polygon") {
-                            // Iterates the root coordinates (List<object>), and casts each element in the list to a Jarray, 
-                            // then casts each Jarray's element to another Jarray which contain the actual coordinates.
-                            for (int i = 0; i < feature.geometry.coordinates.Count; i++) {
-                                for (int j = 0; j < ((JArray)feature.geometry.coordinates[i]).Count; j++) {
-                                    trueCoords = ((JArray)(((JArray)feature.geometry.coordinates[i])[j])).ToObject<object[]>();
-                                    feature.properties.xamarincoordinates.Add(JsonCoordToXamarinPoint(trueCoords));
-                                }
-                            }
-                        } else {
-                            Debug.WriteLine($"\n\n::::::::::::::::::::INVALID TYPE WHEN PARSING POINTS: {feature.geometry.type}");
-                            throw new Exception();
-                        }
-                    }
+                    await TryParseFeature(feature);
                 }
+
                 return rootobject.features;
             });
+        }
+
+        private async Task<bool> TryParseFeature(Feature feature) {
+
+            // Ensure the feature has valid GeoJSON fields supplied.
+            if(feature == null || feature.type == null || feature.geometry == null || feature.geometry.type == null || feature.geometry.coordinates == null) {
+                await HomePage.Instance.DisplayAlert("Invalid File Contents", "Please make sure your GeoJSON is formatted correctly.", "OK");
+                return false;
+            }
+
+            // Immediately convert LineStrings to Line for use in the rest of the codebase. 
+            // This will be converted back to LineString before serialization back to json.
+            if (feature.geometry.type == "LineString") {
+                feature.geometry.type = "Line";
+            }
+
+            // If author ID hasn't been set on the feature, default it to the user's ID.
+            if (string.IsNullOrWhiteSpace(feature.properties.authorId)) {
+                if (App.Current.Properties.ContainsKey("UserID")) {
+                    feature.properties.authorId = App.Current.Properties["UserID"] as string;
+                } else {
+                    feature.properties.authorId = string.Empty;
+                }
+            }
+
+            // If the date field is missing or invalid, convert it into DateTime.Now.
+            DateTime dummy;
+            if (feature.properties.date == null || DateTime.TryParse(feature.properties.date, out dummy) == false) {
+                Debug.WriteLine($"\n\n::::::::::::::::::::::BAD DATE VALUE: {feature.properties.date}, DEFAULTING TO DateTime.Now");
+                feature.properties.date = DateTime.Now.ToShortDateString();
+            }
+
+            // Determine the icon used for each feature based on it's geometry type.
+            if (feature.geometry.type == "Point") {
+                feature.properties.typeIconPath = "point_icon.png";
+            } else if (feature.geometry.type == "Line") {
+                feature.properties.typeIconPath = "line_icon.png";
+            } else if (feature.geometry.type == "Polygon") {
+                feature.properties.typeIconPath = "area_icon.png";
+            } else {
+                await HomePage.Instance.DisplayAlert("Import Error", "Groundsman currently only supports feature types of Point, Line, and Polygon", "OK");
+                return false;
+            }
+
+            // Initialise xamarin coordinates list.
+            feature.properties.xamarincoordinates = new List<Point>();
+
+            // Properly deserialize the list of coordinates into an app-use-specific list of Points (XamarinCoordinates).
+            {
+                object[] trueCoords;
+
+                if (feature.geometry.type == "Point") {
+                    trueCoords = feature.geometry.coordinates.ToArray();
+                    feature.properties.xamarincoordinates.Add(JsonCoordToXamarinPoint(trueCoords));
+
+                } else if (feature.geometry.type == "Line") {
+                    // Iterates the root coordinates (List<object>),
+                    // then casts each element in the list to a Jarray which contain the actual coordinates.
+                    for (int i = 0; i < feature.geometry.coordinates.Count; i++) {
+                        trueCoords = ((JArray)feature.geometry.coordinates[i]).ToObject<object[]>();
+                        feature.properties.xamarincoordinates.Add(JsonCoordToXamarinPoint(trueCoords));
+                    }
+                } else if (feature.geometry.type == "Polygon") {
+                    // Iterates the root coordinates (List<object>), and casts each element in the list to a Jarray, 
+                    // then casts each Jarray's element to another Jarray which contain the actual coordinates.
+                    for (int i = 0; i < feature.geometry.coordinates.Count; i++) {
+                        for (int j = 0; j < ((JArray)feature.geometry.coordinates[i]).Count; j++) {
+                            trueCoords = ((JArray)(((JArray)feature.geometry.coordinates[i])[j])).ToObject<object[]>();
+                            feature.properties.xamarincoordinates.Add(JsonCoordToXamarinPoint(trueCoords));
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -281,8 +294,8 @@ namespace GeoApp {
         public async Task<bool> ImportFeaturesFromFile(string path) {
             try {
                 String text = File.ReadAllText(path);
-                await ImportFeaturesAsync(text);
-                return true;
+                bool resultStatus = await ImportFeaturesAsync(text);
+                return resultStatus;
             } catch (Exception ex) {
                 await HomePage.Instance.DisplayAlert("Invalid File", "An unknown error occured when trying to process this file.", "OK");
                 Debug.WriteLine(ex);
@@ -297,18 +310,29 @@ namespace GeoApp {
         /// <returns></returns>
         public async Task<bool> ImportFeaturesAsync(string fileContents) {
             try {
+                // Ensure file contents are structured in a valid GeoJSON format.
                 var importedRootObject = JsonConvert.DeserializeObject<RootObject>(fileContents);
                 if(importedRootObject == null) {
-                    Debug.WriteLine($"\n\n:::::::::::::::::DESERIALIZATION FAILED");
+                    await HomePage.Instance.DisplayAlert("Invalid File Contents", "Please make sure your GeoJSON is formatted correctly.", "OK");
                     return false;
+                }
+
+                // Loop through all imported features and make sure they are valid.
+                foreach (var importedFeature in importedRootObject.features) {
+                    bool parseResult = await TryParseFeature(importedFeature);
+
+                    if (parseResult == false) {
+                        return false;
+                    }
                 }
 
                 // Loop through all imported features one by one, ensuring there are no ID clashes.
                 foreach (var importedFeature in importedRootObject.features) {
-                    Debug.WriteLine($"\n\n:::::::::::::::::DESERIALIZING NOW");
                     TryGetUniqueFeatureID(importedFeature);
-                    App.FeaturesManager.CurrentFeatures.Add(importedFeature);
                 }
+
+                // Finally, add all the imported features to the current features list.
+                App.FeaturesManager.CurrentFeatures.AddRange(importedRootObject.features);
 
                 await SaveCurrentFeaturesToEmbeddedFile();
                 await HomePage.Instance.DisplayAlert("File", "Import Success!", "OK");
