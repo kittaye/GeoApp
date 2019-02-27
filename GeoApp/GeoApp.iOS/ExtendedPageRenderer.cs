8fd6147f8453be0274a2511935b6f62b399279a4
Reilly MacKenzie-Cree <n9966471@qut.edu.au>
2019-02-27 21:25:38 +1000

    Updated app icons, movied profile icon on ios

--- GeoApp/GeoApp.iOS/ExtendedPageRenderer.cs
+++ GeoApp/GeoApp.iOS/ExtendedPageRenderer.cs
@@ -10,48 +10,64 @@ namespace GeoApp.iOS
 {
     public class ExtendedPageRenderer : PageRenderer
     {
-        public override void ViewWillAppear(bool animated)
+        public override void ViewDidAppear(bool animated)
         {
-            base.ViewWillAppear(animated);
+            base.ViewDidAppear(animated);
+
+            if (NavigationController != null)
+            {
+                NavigationController.NavigationBar.TintColor = UIColor.FromRGB(76, 175, 80);
+            }
+                var contentPage = this.Element as ContentPage;
+                if (contentPage == null || NavigationController == null)
+                    return;
+
+                var itemsInfo = contentPage.ToolbarItems;
+
+                var navigationItem = this.NavigationController.TopViewController.NavigationItem;
+                var leftNativeButtons = (navigationItem.LeftBarButtonItems ?? new UIBarButtonItem[] { }).ToList();
+                var rightNativeButtons = (navigationItem.RightBarButtonItems ?? new UIBarButtonItem[] { }).ToList();
+
+                var newLeftButtons = new UIBarButtonItem[] { }.ToList();
+                var newRightButtons = new UIBarButtonItem[] { }.ToList();
+
+                rightNativeButtons.ForEach(nativeItem =>
+                {
+                // [Hack] Get Xamarin private field "item"
+                var field = nativeItem.GetType().GetField("_item", BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (field == null)
+                        return;
+
+                    var info = field.GetValue(nativeItem) as ToolbarItem;
+                    if (info == null)
+                        return;
+
+                    if (info.Priority == 1)
+                        newLeftButtons.Add(nativeItem);
+                    else
+                        newRightButtons.Add(nativeItem);
+                });
+
+                leftNativeButtons.ForEach(nativeItem =>
+                {
+                    newLeftButtons.Add(nativeItem);
+                });
+
+                navigationItem.RightBarButtonItems = newRightButtons.ToArray();
+                navigationItem.LeftBarButtonItems = newLeftButtons.ToArray();
+            }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
 
             var contentPage = this.Element as ContentPage;
             if (contentPage == null || NavigationController == null)
                 return;
 
-            var itemsInfo = contentPage.ToolbarItems;
-
             var navigationItem = this.NavigationController.TopViewController.NavigationItem;
-            var leftNativeButtons = (navigationItem.LeftBarButtonItems ?? new UIBarButtonItem[] { }).ToList();
-            var rightNativeButtons = (navigationItem.RightBarButtonItems ?? new UIBarButtonItem[] { }).ToList();
-
-            var newLeftButtons = new UIBarButtonItem[] { }.ToList();
-            var newRightButtons = new UIBarButtonItem[] { }.ToList();
-
-            rightNativeButtons.ForEach(nativeItem =>
-            {
-                // [Hack] Get Xamarin private field "item"
-                var field = nativeItem.GetType().GetField("_item", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (field == null)
-                    return;
-
-                var info = field.GetValue(nativeItem) as ToolbarItem;
-                if (info == null)
-                    return;
-
-                if (info.Priority == 1)
-                    newLeftButtons.Add(nativeItem);
-                else
-                    newRightButtons.Add(nativeItem);
-            });
-
-            leftNativeButtons.ForEach(nativeItem =>
-            {
-                newLeftButtons.Add(nativeItem);
-            });
-
-            navigationItem.RightBarButtonItems = newRightButtons.ToArray();
-            navigationItem.LeftBarButtonItems = newLeftButtons.ToArray();
+            navigationItem.LeftBarButtonItems = null;
         }
     }
 
-}
+    }
